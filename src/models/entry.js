432b07989110d378de52e2c247d1fd6672f609a9
Ilya Shatalov <Ilya.Shatalov@gmail.com>
2023-01-04 18:21:25 +0400

    fix: TTL for keys
    mongoose schema dont accept strings on expires field

--- src/models/entry.js
+++ src/models/entry.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const KEY_TTL = parseInt(process.env.KEY_TTL);
 
 var entrySchema = mongoose.Schema({
-    key: {
-        type: String,
-        required: true,
-        unique : true
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  updatedAt: {
+    expires: KEY_TTL,
+    type: Date,
+    default: () => {
+      return new Date();
     },
-    value: { 
-        type: String,
-        required: true
-    },
-    updatedAt: {
-        type: Date,
-        default: () => { return new Date() }
-    }
+  },
 });
 
-module.exports = mongoose.model("Entry", entrySchema); 
+module.exports = mongoose.model("Entry", entrySchema);
