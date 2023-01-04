6ae0f8e8f611d1e4818177118e61aa5239be1cfc
Ilya Shatalov <Ilya.Shatalov@gmail.com>
2023-01-04 18:18:37 +0400

    refactor: docs: split into two func and docs

--- src/utils/cache-tools.js
+++ src/utils/cache-tools.js
@@ -6,6 +6,25 @@ const Entry = require(path.join(process.cwd(), "models", "entry"));
 require("dotenv").config();
 const CACHE_MAX_COUNT = process.env.CACHE_MAX_COUNT;
 
+/**
+ * Takes key and value and trying to update value of @key
+ * If no @key found check for count of keys and if it's more
+ * then CACHE_MAX_COUNT (check it in .env file) deletes
+ * last updated item
+ */
+
+async function checkAndPop() {
+  const count = await Entry.count();
+  if (count >= CACHE_MAX_COUNT) {
+    logger.debug("There're more then + CACHE_MAX_COUNT");
+    const entry = await Entry.findOneAndDelete(
+      {},
+      { sort: { updatedAt: 1 }, limit: 1 }
+    );
+    logger.info("Deleted: " + entry);
+  }
+}
+
 async function updateOrPopAddEntry(key, value, callback) {
   const filter = { key: key };
   const update = { value: value, updatedAt: new Date() };
@@ -16,12 +35,8 @@ async function updateOrPopAddEntry(key, value, callback) {
       return callback(null, result);
     }
 
-    const count = await Entry.count();
-    if (count >= CACHE_MAX_COUNT) {
-      logger.debug("There're more then 10  "+ count);
-      const entry = await Entry.findOneAndDelete({}, { sort: {updatedAt: 1 }, limit: 1});
-      logger.info("Deleted: "+ entry);
-    }
+    await checkAndPop();
+
     const newEntry = new Entry({
       key,
       value,
@@ -29,7 +44,7 @@ async function updateOrPopAddEntry(key, value, callback) {
     await newEntry.save();
     return callback(null, newEntry);
   } catch (err) {
-    logger.error(err);
+    logger.error(err, err.stack);
     return callback(err, null);
   }
 }
